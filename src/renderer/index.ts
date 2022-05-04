c4efb7b4c28645679bb3da5c43628d117be489e6
i18furuta <fn.nk.2002@gmail.com>
2022-05-04 17:19:15 +0900

    ファイルロード中に処理中メッセージを表示するようにした

--- src/renderer/index.ts
+++ src/renderer/index.ts
@@ -8,13 +8,16 @@ const canvas = c as HTMLCanvasElement;
 const ctx = canvas.getContext('2d');
 assertIsDefined(ctx);
 
+const tac = new Tac(ctx);
+
 const openBtn = document.getElementById('btn-openFile');
 assertIsDefined(openBtn);
 
 const filePathElement = document.getElementById('filePath');
 assertIsDefined(filePathElement);
 
-const tac = new Tac(ctx);
+const loadingMsg = document.getElementById('loading');
+assertIsDefined(loadingMsg);
 
 // 「Open a File」ボタンが押された時の動作
 openBtn.addEventListener('click', async () => {
@@ -24,8 +27,11 @@ openBtn.addEventListener('click', async () => {
   if (filePath === undefined) {
     text = 'ファイルが選択されていません';
   } else {
+    // 「しばらくお待ちください」を表示する
+    loadingMsg.style.display = 'block';
     window.electronAPI.openFile(filePath);
     text = filePath;
+    loadingMsg.style.display = 'none';
   }
 
   if (filePathElement !== null) {
