fc179b47bd134343aa3e1f0ecb44c31bbaf679eb
i18furuta <fn.nk.2002@gmail.com>
2022-05-15 13:10:03 +0900

    MemoryクラスにもIDataBusを実装させた

--- src/renderer/TaC/memory/memory.ts
+++ src/renderer/TaC/memory/memory.ts
@@ -1,12 +1,15 @@
+import { IDataBus } from '../interface';
 import { IDmaSignal } from '../interface/dmaSignal';
 
 const MEMORY_SIZE = 64 * 1024;
 
-export class Memory implements IDmaSignal {
+export class Memory implements IDmaSignal, IDataBus {
   private mem: Uint8Array;
+  private size: number;
 
   constructor() {
-    this.mem = new Uint8Array(MEMORY_SIZE);
+    this.size = MEMORY_SIZE;
+    this.mem = new Uint8Array(this.size);
   }
 
   write8(addr: number, val: number) {
@@ -15,5 +18,18 @@ export class Memory implements IDmaSignal {
 
   read8(addr: number) {
     return this.mem[addr];
+  }
+
+  write16(addr: number, val: number) {
+    this.write8(addr, (val & 0xff00) >> 8);
+    this.write8(addr + 1, val & 0x00ff);
+  }
+
+  read16(addr: number): number {
+    return (this.read8(addr) << 8) | this.read8(addr + 1);
+  }
+
+  getMemorySize() {
+    return this.size;
   }
 }
