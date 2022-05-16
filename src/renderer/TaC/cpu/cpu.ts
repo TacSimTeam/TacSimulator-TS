866ba0c184d9ca90a287dc9e038df8964f9bd6f1
i18furuta <fn.nk.2002@gmail.com>
2022-05-16 17:23:04 +0900

    実効アドレス計算実装

--- src/renderer/TaC/cpu/cpu.ts
+++ src/renderer/TaC/cpu/cpu.ts
@@ -1,4 +1,4 @@
-import { Register } from './register';
+import { Register, REGISTER_FP } from './register';
 import { IDataBus } from '../interface/dataBus';
 import * as operation from './operation';
 
@@ -7,7 +7,8 @@ type Instruction = {
   addrMode: number; // アドレッシングモード
   rd: number; // ディスティネーションレジスタ
   rx: number; // インデクスレジスタ
-  dsp: number; // オペランド
+  dsp: number; // 実効アドレス
+  operand: number; // オペランド
 };
 
 const FLAG_E = 0x80;
@@ -17,6 +18,15 @@ const FLAG_V = 0x08;
 const FLAG_C = 0x04;
 const FLAG_S = 0x02;
 const FLAG_Z = 0x01;
+
+const ADDRMODE_DIRECT = 0;
+const ADDRMODE_INDEXED = 1;
+const ADDRMODE_IMMEDIATE = 2;
+const ADDRMODE_FP_RELATIVE = 3;
+const ADDRMODE_REG_TO_REG = 4;
+const ADDRMODE_SHORT_IMMEDIATE = 5;
+const ADDRMODE_REG_INDIRECT = 6;
+const ADDRMODE_BYTE_REG_INDIRECT = 7;
 
 export class Cpu {
   private flag: number;
@@ -36,9 +46,13 @@ export class Cpu {
     for (;;) {
       // fetch
       const data = this.memory.read16(this.pc);
+      this.nextPC();
 
       // decode
       const inst = this.decode(data);
+
+      // effective address calculation
+      inst.dsp = this.calcEffectiveAddress(inst.addrMode, inst.rx);
     }
   }
 
@@ -49,9 +63,40 @@ export class Cpu {
       rd: (data >>> 4) & 0x0f,
       rx: data & 0x0f,
       dsp: 0,
+      operand: 0,
     };
 
     return inst;
+  }
+
+  /* valを符号付4bit整数に変換する */
+  private convSignedInt4(val: number) {
+    if ((val & 0x08) !== 0) {
+      val = (val & 0x07) - 8;
+    }
+    return val;
+  }
+
+  /**
+   * 実効アドレス計算
+   *
+   * @param addrMode アドレッシングモード
+   * @param rx インデクスレジスタ
+   * @returns 対象となるアドレス. 1ワード命令の時は0を返す
+   */
+  private calcEffectiveAddress(addrMode: number, rx: number) {
+    const data = this.memory.read16(this.pc);
+    switch (addrMode) {
+      case ADDRMODE_DIRECT:
+        return data;
+      case ADDRMODE_INDEXED:
+        return data + this.register.readReg(rx);
+      case ADDRMODE_FP_RELATIVE:
+        // console.log(`rx : ${rx}, this.convSignedInt4(rx) : ${this.convSignedInt4(rx)}`);
+        return this.register.readReg(REGISTER_FP) + this.convSignedInt4(rx) * 2;
+      default:
+        return 0;
+    }
   }
 
   private execInstruction(inst: Instruction) {
@@ -60,5 +105,19 @@ export class Cpu {
         console.log('NOP');
         break;
     }
+  }
+
+  private nextPC() {
+    this.pc += 2;
+  }
+
+  /* テスト用関数 */
+  private setRegister(num: number, val: number) {
+    this.register.writeReg(num, val);
+  }
+
+  /* テスト用関数 */
+  private setPC(pc: number) {
+    this.pc = pc;
   }
 }
--- test/cpu.test.ts
+++ test/cpu.test.ts
@@ -1,6 +1,7 @@
 import { Cpu } from '../src/renderer/TaC/cpu/cpu';
 import { Mmu } from '../src/renderer/TaC/memory/mmu';
 import { Memory } from '../src/renderer/TaC/memory/memory';
+import { REGISTER_FP, REGISTER_G2, REGISTER_G3 } from '../src/renderer/TaC/cpu/register';
 
 test('CPU decode Test', () => {
   const cpu = new Cpu(new Mmu(new Memory()));
@@ -11,4 +12,55 @@ test('CPU decode Test', () => {
   expect(inst.rd).toBe(0b1010);
   expect(inst.rx).toBe(0b0101);
   expect(inst.dsp).toBe(0);
+});
+
+test('Converting unsigned int4 to signed int4', () => {
+  const cpu = new Cpu(new Mmu(new Memory()));
+
+  expect(cpu['convSignedInt4'](0b0000)).toBe(0);
+  expect(cpu['convSignedInt4'](0b0001)).toBe(1);
+  expect(cpu['convSignedInt4'](0b0010)).toBe(2);
+  expect(cpu['convSignedInt4'](0b0011)).toBe(3);
+  expect(cpu['convSignedInt4'](0b0100)).toBe(4);
+  expect(cpu['convSignedInt4'](0b0101)).toBe(5);
+  expect(cpu['convSignedInt4'](0b0110)).toBe(6);
+  expect(cpu['convSignedInt4'](0b0111)).toBe(7);
+  expect(cpu['convSignedInt4'](0b1000)).toBe(-8);
+  expect(cpu['convSignedInt4'](0b1001)).toBe(-7);
+  expect(cpu['convSignedInt4'](0b1010)).toBe(-6);
+  expect(cpu['convSignedInt4'](0b1011)).toBe(-5);
+  expect(cpu['convSignedInt4'](0b1100)).toBe(-4);
+  expect(cpu['convSignedInt4'](0b1101)).toBe(-3);
+  expect(cpu['convSignedInt4'](0b1110)).toBe(-2);
+  expect(cpu['convSignedInt4'](0b1111)).toBe(-1);
+});
+
+test('CPU effective address calculation', () => {
+  const mmu = new Mmu(new Memory());
+  const cpu = new Cpu(mmu);
+
+  /* LD G3,0x1234 */
+  cpu['setPC'](2);
+  mmu.write16(0x0000, 0b0000100001100000); // Addr : 0x0000
+  mmu.write16(0x0002, 0b0001001000110100); // Addr : 0x0002
+  expect(cpu['calcEffectiveAddress'](0, REGISTER_G3)).toBe(0x1234);
+
+  /* LD G1,0x1234,G2(5) */
+  cpu['setPC'](6);
+  mmu.write16(0x0004, 0b0000100100010010); // Addr : 0x0004
+  mmu.write16(0x0006, 0b0001001000110100); // Addr : 0x0006
+  cpu['setRegister'](REGISTER_G2, 5);
+  expect(cpu['calcEffectiveAddress'](1, REGISTER_G2)).toBe(0x1234 + 5);
+
+  /* LD SP,#0x1234 */
+  cpu['setPC'](10);
+  mmu.write16(0x0008, 0b0000101011010000); // Addr : 0x0008
+  mmu.write16(0x000a, 0b0001001000110100); // Addr : 0x000a
+  expect(cpu['calcEffectiveAddress'](2, REGISTER_G2)).toBe(0);
+
+  /* LD Rd,-4(-2*2),FP (FP相対は1ワード命令) */
+  cpu['setPC'](12);
+  mmu.write16(0x000c, 0b0000101100111110); // Addr : 0x000c
+  cpu['setRegister'](REGISTER_FP, 0x1234);
+  expect(cpu['calcEffectiveAddress'](3, 0b1110)).toBe(0x1234 - 4);
 });
