9bb3ad6cc715c78dfbb773ec56f1e59af53359c9
i18furuta <fn.nk.2002@gmail.com>
2022-09-28 10:28:13 +0900

    TLBUtilテスト修正

--- test/tlbUtil.test.ts
+++ test/tlbUtil.test.ts
@@ -5,13 +5,15 @@ test('tlbObjToNum() Test', () => {
     page: 0x22,
     frame: 0xcc,
     validFlag: true,
+    undefinedFlag1: true,
+    undefinedFlag2: false,
     referenceFlag: false,
     dirtyFlag: true,
     readFlag: false,
     writeFlag: true,
     executeFlag: false,
   };
-  let entryNum = 0x00228acc;
+  let entryNum = 0x0022cacc;
 
   expect(tlbObjToNum(entryObj)).toBe(entryNum);
 
@@ -19,23 +21,27 @@ test('tlbObjToNum() Test', () => {
     page: 0xaa,
     frame: 0xdd,
     validFlag: false,
+    undefinedFlag1: false,
+    undefinedFlag2: true,
     referenceFlag: true,
     dirtyFlag: false,
     readFlag: true,
     writeFlag: false,
     executeFlag: true,
   };
-  entryNum = 0x00aa15dd;
+  entryNum = 0x00aa35dd;
 
   expect(tlbObjToNum(entryObj)).toBe(entryNum);
 });
 
 test('tlbNumToObj() Test', () => {
-  const entryNum = 0x00aa15ee;
+  const entryNum = 0x00aa75ee;
   const entryObj: TlbEntry = {
     page: 0xaa,
     frame: 0xee,
     validFlag: false,
+    undefinedFlag1: true,
+    undefinedFlag2: true,
     referenceFlag: true,
     dirtyFlag: false,
     readFlag: true,
