fa47a2667970bdfa3b00170bc7b9f7610800b761
i18furuta <fn.nk.2002@gmail.com>
2022-11-11 11:10:01 +0900

    16進表示用の便利関数定義

--- src/renderer/utils.ts
+++ src/renderer/utils.ts
@@ -12,3 +12,25 @@ export function assertIsDefined<T>(val: T): asserts val is NonNullable<T> {
     throw new Error(`Expected 'val' to be defined, but received ${val}`);
   }
 }
+
+/**
+ * 数値を16進数表現の文字列に変換する
+ * 先頭に'0x'が付く
+ *
+ * @param val 変換したい数値
+ * @param maxByte 表示したいバイト数(省略すると2)
+ *                例) val = 0x23, maxByte = 2のときは
+ *                    '0x0023'のように2バイト長になるように0埋めする
+ * @returns 16進数に変換した文字列
+ */
+export function toHexString(val: number, prefix?: string, maxByte?: number): string {
+  if (prefix === undefined) {
+    prefix = '0x';
+  }
+
+  if (maxByte === undefined) {
+    maxByte = 2;
+  }
+
+  return prefix + val.toString(16).padStart(maxByte * 2, '0');
+}
---
+++ test/util.test.ts
@@ -0,0 +1,18 @@
+import { toHexString } from '../src/renderer/utils';
+
+test('Convert number to hex string test', () => {
+  expect(toHexString(0x0001)).toBe('0x0001');
+  expect(toHexString(0x0011)).toBe('0x0011');
+  expect(toHexString(0x0111)).toBe('0x0111');
+  expect(toHexString(0x1111)).toBe('0x1111');
+
+  expect(toHexString(0x0001, 'abc')).toBe('abc0001');
+  expect(toHexString(0x0011, 'abc')).toBe('abc0011');
+  expect(toHexString(0x0111, 'abc')).toBe('abc0111');
+  expect(toHexString(0x1111, 'abc')).toBe('abc1111');
+
+  expect(toHexString(0x0001, '0x', 4)).toBe('0x00000001');
+  expect(toHexString(0x0011, '0x', 4)).toBe('0x00000011');
+  expect(toHexString(0x0111, '0x', 4)).toBe('0x00000111');
+  expect(toHexString(0x1111, '0x', 4)).toBe('0x00001111');
+});
