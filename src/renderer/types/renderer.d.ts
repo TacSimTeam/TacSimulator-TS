5eea2ac0c8a72393fa6819b331b1bdd5038d983e
i18furuta <fn.nk.2002@gmail.com>
2022-12-21 16:09:15 +0900

    renderer.d.ts修正

--- src/renderer/types/renderer.d.ts
+++ src/renderer/types/renderer.d.ts
@@ -1,10 +1,10 @@
 /* レンダラープロセスに公開するAPIの型定義 */
 export interface IElectronAPI {
-  readSct: (sctAddr: number) => Promise<Uint8Array>;
-  writeSct: (sctAddr: number, data: Uint8Array) => Promise<void>;
-  openFile: (filePath: string) => Promise<void>;
-  isSDImgLoaded: () => boolean;
-  getSDImgPath: () => Promise<string | undefined>;
+  readSct(sctAddr: number): Promise<Uint8Array>;
+  writeSct(sctAddr: number, data: Uint8Array): Promise<void>;
+  openFile(filePath: string): Promise<void>;
+  isSDImgLoaded(): boolean;
+  getSDImgPath(): Promise<string | undefined>;
 }
 
 declare global {
