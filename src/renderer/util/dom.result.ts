5b809ee2c012c98c04ac5b5beb97d009ed731715
i18furuta <fn.nk.2002@gmail.com>
2022-12-21 16:13:20 +0900

    コメント追加

--- src/renderer/util/dom.result.ts
+++ src/renderer/util/dom.result.ts
@@ -1,5 +1,11 @@
 import { Failure, Result, Success } from './result';
 
+/**
+ * Resultで要素を返すdocument.querySelector()のラッパー関数
+ *
+ * @param selectors セレクター(id, class, nameなど)
+ * @returns Result<取得したHTML要素, undefined>
+ */
 export function querySelector<Element extends HTMLElement = HTMLElement>(
   selectors: string
 ): Result<Element, undefined> {
