0534a2846b19b7b5a0c9c391901722f338135c14
i18furuta <fn.nk.2002@gmail.com>
2022-12-22 10:14:30 +0900

    lib.ts微修正

--- src/renderer/util/lib.ts
+++ src/renderer/util/lib.ts
@@ -6,16 +6,8 @@
  * @param maxByte 表示したいバイト数(省略すると2)
  *                例) val = 0x23, maxByte = 2のときは
  *                    '0x0023'のように2バイト長になるように0埋めする
- * @returns 16進数に変換した文字列
+ * @return 16進数に変換した文字列
  */
-export function toHexString(val: number, prefix?: string, maxByte?: number): string {
-  if (prefix === undefined) {
-    prefix = '0x';
-  }
-
-  if (maxByte === undefined) {
-    maxByte = 2;
-  }
-
+export function toHexString(val: number, prefix: string = '0x', maxByte: number = 2): string {
   return prefix + val.toString(16).padStart(maxByte * 2, '0');
 }
