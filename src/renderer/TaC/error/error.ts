9b79548ed5a0720322888cf679b0781cecd80e8a
i18furuta <fn.nk.2002@gmail.com>
2022-12-23 10:21:10 +0900

    error.tsのリファクタリング

--- src/renderer/TaC/error/error.ts
+++ src/renderer/TaC/error/error.ts
@@ -2,7 +2,8 @@ class BaseError extends Error {
   constructor(e?: string) {
     super(e);
     this.name = new.target.name;
-    /* 下記の行はTypeScriptの出力ターゲットがES2015より古い場合(ES3, ES5)のみ必要 */
+
+    // 下記の行はTypeScriptの出力ターゲットがES2015より古い場合(ES3, ES5)のみ必要
     Object.setPrototypeOf(this, new.target.prototype);
   }
 }
